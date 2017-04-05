439f3b1177a24b82e894a1bf2af356fc934aa464
Paul Joachim Bloch Thorsen <Paul Joachim Bloch Thorsen>
2017-04-05 23:48:59 +0200

    logos

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'SCREEN',
+      script    : 'src/index.js',
+      env : {
+        NODE_ENV: 'screen'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'STREAM',
+      script    : 'src/index.js',
+      env : {
+        NODE_ENV: 'stream'
+      }
+    },
+  ]
+};
