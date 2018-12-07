57fe086c66089abc04ee02a557b628819c4837ae
Peder Hans Øverland <peder.overland@knowit.no>
2018-12-07 14:02:39 +0100

    Add production deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,38 @@ module.exports = {
       name: "SCREEN",
       script: "src/index.js",
       env: {
-        NODE_ENV: "screen"
-      }
+        NODE_ENV: "screen",
+        port: 10000
+      },
     }
-  ]
+  ],
+  deploy: {
+    // "production" is the environment name
+    production: {
+      // SSH key path, default to $HOME/.ssh
+      key: "/path/to/some.pem",
+      // SSH user
+      user: "peder",
+      // SSH host
+      host: ["pm.alveroverland.com"],
+      // SSH options with no command-line flag, see 'man ssh'
+      // can be either a single string or an array of strings
+      ssh_options: "StrictHostKeyChecking=no",
+      // GIT remote/branch
+      ref: "origin/master",
+      // GIT remote
+      repo: "git@github.com:pederOverland/divecalcresults.git",
+      // path in the server
+      path: "/var/www/divecalcresults",
+      // Pre-setup command or path to a script on your local machine
+      // 'pre-setup': "apt-get install git ; ls -la",
+      // Post-setup commands or path to a script on the host machine
+      // eg: placing configurations in the shared dir etc
+      'post-setup': "ls -la",
+      // pre-deploy action
+      'pre-deploy-local': "echo 'This is a local executed command'",
+      // post-deploy action
+      'post-deploy': "npm install",
+    },
+  }
 };
