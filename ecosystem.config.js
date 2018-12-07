b64969c07b4330df18e2b312b6ebd5cde9f004c1
Peder Hans Øverland <peder.overland@knowit.no>
2018-12-07 14:54:25 +0100

    Compile resources

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -38,7 +38,7 @@ module.exports = {
       // pre-deploy action
       'pre-deploy-local': "echo 'This is a local executed command'",
       // post-deploy action
-      'post-deploy': "npm install && npm start",
+      'post-deploy': "npm install && node ./fuse.js && npm start",
     },
   }
 };
